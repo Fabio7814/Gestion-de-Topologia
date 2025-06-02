e232d1927ae431361fdfe944a5b33a9c392347bd
Giordano0313 <giordano.collazos@unmsm.edu.pe>
2025-06-01 22:36:48 -0500

    Se agrego un apartado al main.py

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
 nombre = input("¿Cómo te llamas? ")
 
 # Imprime un saludo personalizado
-print("¡Hola, " + nombre + "! Bienvenido a Python.")
+print("¡Hola, " + nombre y apellidos + "! Bienvenido a Python.")
 
